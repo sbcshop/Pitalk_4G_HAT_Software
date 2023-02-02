7c05a5ffee7c09d0b316c7774cf7db90a99c08c4
sbcshop2 <120171682+sbcshop2@users.noreply.github.com>
2023-02-02 15:12:36 +0530

    Update ReceiveMessage.py

--- Examples/ReceiveMessage.py
+++ Examples/ReceiveMessage.py
@@ -36,9 +36,6 @@ from Library import PiTalk_4G
 
 
 
-
-
-
-
 rcv_msg = PiTalk_4G.eg25().ReceiveMessage()                         #Uncomment this code line to recieve message/recent information about sim card activities
+
 
