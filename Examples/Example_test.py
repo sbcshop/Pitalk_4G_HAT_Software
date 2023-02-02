3be75664a0ad11afcf074e9820134a8857f49f34
sbcshop2 <120171682+sbcshop2@users.noreply.github.com>
2023-02-02 14:40:12 +0530

    Update Example_test.py

--- Examples/Example_test.py
+++ Examples/Example_test.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import serial
 import time
-from PiTalk_4G import eg25
+from Library import PiTalk_4G
+
 
 phone_number = '9118520507' #Enter your mobile number
 time = 100                  #Enter call duration time
@@ -11,13 +12,13 @@ Port = 80      #Enter Port
 ServerIP = ''  #Enter your server IP
 APN = 'airtelgprs.com' #Enter your APN 
 
-#calling = eg25().Call(phone_number, time)  #Uncomment this function line to use call functionality
+#calling = PiTalk_4G.eg25().Call(phone_number, time)  #Uncomment this function line to use call functionality
 
 
-#Send_msg = eg25().SendSMessage(phone_number, text_message) #Uncomment this code line to send text message
+#Send_msg = PiTalk_4Geg25().SendSMessage(phone_number, text_message) #Uncomment this code line to send text message
 
-#rcv_msg = eg25().ReceiveMessage()                         #Uncomment this code line to recieve message/recent information about sim card activities
+#rcv_msg = ePiTalk_4Gg25().ReceiveMessage()                         #Uncomment this code line to recieve message/recent information about sim card activities
 
 #tcp  = eg25().TCP(ServerIP, Port, APN,5)                  #Uncomment this code line to use funtionality of tcp
 
-gps = eg25().gps_positioning()                         #Uncomment this code line to get gps positining 
+gps = PiTalk_4G.eg25().gps_positioning()   
